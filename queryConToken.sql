fe5ed112e7f8e2627e84faefeae34ad9fa2d0356
German Acuna <germanacuna011@gmail.com>
2025-07-08 11:56:17 -0600

    con el script mysql

---
+++ queryConToken.sql
@@ -0,0 +1,19 @@
+use fastApi2;
+
+insert into rol values("1","USUARIO","1","alex","alex", '2000-01-01', '2001-01-01');
+
+INSERT INTO usuario(NAME_USER,AP_PATERNO,AP_MATERNO,CONTRASEÑA,EMAIL_USER) VALUES(
+	"Manu",
+	"The",
+	"Tiger",
+	"1234",
+	"manu@gmail.com"
+);
+
+INSERT INTO usuario(NAME_USER,AP_PATERNO,AP_MATERNO,CONTRASEÑA,EMAIL_USER) VALUES(
+	"Alex",
+	"The",
+	"Lion",
+	"1234",
+	"alex@gmail.com"
+);
